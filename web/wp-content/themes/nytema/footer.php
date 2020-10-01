aca859fa4e79cf9d91f68f2872941b5c301ab1d6
Khalaf27 <43336943+Khalaf27@users.noreply.github.com>
2020-10-01 21:52:40 +0200

    Initial commit

---
+++ web/wp-content/themes/nytema/footer.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package nytema
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'nytema' ) ); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'nytema' ), 'WordPress' );
+				?>
+			</a>
+			<span class="sep"> | </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'nytema' ), 'nytema', '<a href="http://underscores.me/">Underscores.me</a>' );
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+<!-- Font awesome -->
+<script src="https://kit.fontawesome.com/04079c9f85.js" crossorigin="anonymous"></script>
+</body>
+</html>
